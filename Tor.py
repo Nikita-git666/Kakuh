52df1f952c1b124875f1e19e03b3473d9e7e2a54
Nikita-git666 <135269505+Nikita-git666@users.noreply.github.com>
2024-04-21 13:06:35 +0300

    Update Tor.py

--- Tor.py
+++ Tor.py
@@ -1,24 +1,23 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Описание вашей программы')
-subparsers = parser.add_subparsers(help='Доступные подкоманды')
+def start_interface():
+    print("Запуск интерфейса...")
 
-# Подкоманда 1
-parser_cmd1 = subparsers.add_parser('cmd1', help='Описание команды 1')
-parser_cmd1.add_argument('arg1', type=int, help='Описание аргумента для команды 1')
-parser_cmd1.set_defaults(command='cmd1')
+def main():
+    parser = argparse.ArgumentParser(description='Описание вашей программы')
+    subparsers = parser.add_subparsers(help='Доступные подкоманды')
 
-# Подкоманда 2
-parser_cmd2 = subparsers.add_parser('cmd2', help='Описание команды 2')
-parser_cmd2.add_argument('arg2', type=str, help='Описание аргумента для команды 2')
-parser_cmd2.set_defaults(command='cmd2')
+    # Подкоманда для интерфейса
+    parser_start = subparsers.add_parser('start', help='Запуск интерфейса')
+    parser_start.set_defaults(command='start')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if hasattr(args, 'command'):
-    if args.command == 'cmd1':
-        print("Вы вызвали команду 1 с аргументом:", args.arg1)
-    elif args.command == 'cmd2':
-        print("Вы вызвали команду 2 с аргументом:", args.arg2)
-else:
-    parser.print_help()
+    if hasattr(args, 'command'):
+        if args.command == 'start':
+            start_interface()
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
