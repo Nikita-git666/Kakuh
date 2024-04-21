6da718fa7f7b6ecf69ea8e4f5e78f6873de83b3d
Nikita-git666 <135269505+Nikita-git666@users.noreply.github.com>
2024-04-21 12:55:20 +0300

    Update Tor.py

--- Tor.py
+++ Tor.py
@@ -1,5 +1,24 @@
-while True:
-    user_input = input("Введите команду: ")
-    if user_input.lower() == "start":
-        print("Hello, World!")
-        break
+import argparse
+
+parser = argparse.ArgumentParser(description='Описание вашей программы')
+subparsers = parser.add_subparsers(help='Доступные подкоманды')
+
+# Подкоманда 1
+parser_cmd1 = subparsers.add_parser('cmd1', help='Описание команды 1')
+parser_cmd1.add_argument('arg1', type=int, help='Описание аргумента для команды 1')
+parser_cmd1.set_defaults(command='cmd1')
+
+# Подкоманда 2
+parser_cmd2 = subparsers.add_parser('cmd2', help='Описание команды 2')
+parser_cmd2.add_argument('arg2', type=str, help='Описание аргумента для команды 2')
+parser_cmd2.set_defaults(command='cmd2')
+
+args = parser.parse_args()
+
+if hasattr(args, 'command'):
+    if args.command == 'cmd1':
+        print("Вы вызвали команду 1 с аргументом:", args.arg1)
+    elif args.command == 'cmd2':
+        print("Вы вызвали команду 2 с аргументом:", args.arg2)
+else:
+    parser.print_help()
